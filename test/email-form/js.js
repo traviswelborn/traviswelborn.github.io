9f7efd26ce029d7223c747351c8c9a001112731f
Travis Welborn <traviswelborn@TravisWornsiMac.fios-router.home>
2017-02-27 12:07:47 -0500

    Creation of /test directory.

---
+++ test/email-form/js.js
@@ -0,0 +1,69 @@
+
+
+
+
+// SHOW WHAT'S BEING TYPED
+
+$(".subject").keyup(function(){
+	
+	var subjectValue = $(this).val();
+	$(".subject-p").text(subjectValue);
+	
+}).keyup();
+
+$(".body").keyup(function(){
+	
+	var bodyValue = $(this).val();
+	$(".body-p").text(bodyValue);
+	
+}).keyup();
+
+
+
+
+
+$(".send").click(function(){
+	
+	
+	// GET INPUT AND TEXTAREA VALUES
+	
+	var subjectValue = $(".subject").val();
+	var bodyValue = $(".body").val();
+	
+	
+	// SEE WHAT THE VALUES ARE
+	
+	if (subjectValue == "Your Subject"){
+		
+		var emailSubject = "";
+		
+	} else {
+		
+		var specialCharactersRemover = encodeURIComponent(subjectValue);
+		var emailSubject = specialCharactersRemover;
+		
+	}
+	
+	if (bodyValue == ""){
+		
+		var emailBody = "";
+		
+	} else {
+		
+		var specialCharactersRemover = encodeURIComponent(bodyValue);
+		var emailBody = specialCharactersRemover;
+		
+	}
+	
+	
+	// CREATE MAILTO: QUERIES
+	
+	var hrefValue = $(".send").attr("href");
+	$(".send").attr("href",hrefValue+"?subject="+emailSubject+"&body="+emailBody);
+	
+	
+});
+
+
+
+
