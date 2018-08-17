ebf9804b8241d3f045ceba166afcfda0c5f25e6e
Travis Welborn <traviswelborn@Travis-Welborns-iMac.local>
2018-08-16 20:31:07 -0400

    Testing V6

---
+++ testing/js/scripts.js
@@ -0,0 +1,40 @@
+
+
+// HIDE ELEMENTS
+
+//$(".slide-in-1,.slide-in-2,.slide-in-3").addClass("hidden-slide-in");
+
+
+$(document).ready(function(){
+	
+	
+	// COPYRIGHT YEAR
+	
+	var date=new Date();
+	var year=date.getFullYear();
+	
+	$(".year").html(year);
+	
+	
+	// SHOW ELEMENTS
+	
+	setTimeout(function(){
+		
+		$(".slide-in-1").addClass("show-slide-in").removeClass("hidden-slide-in");
+		
+	},300);
+	
+	setTimeout(function(){
+		
+		$(".slide-in-2").addClass("show-slide-in").removeClass("hidden-slide-in");
+		
+	},600);
+	
+	setTimeout(function(){
+		
+		$(".slide-in-3").addClass("show-slide-in").removeClass("hidden-slide-in");
+		
+	},900);
+		
+	
+});
